0bff708ab7f192487d3b3ee4a456876ad20d95b8
alperenbayraktar <alperenbayraktar96@gmail.com>
2020-05-20 22:37:37 +0300

    Visualization-categorization

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from preprocessing import PreProcessing
+
 ##Use your dataset to construct 3 frequent pattern mining models as follows:
 #i) Apriori.
 #ii) FP-Growth.
@@ -6,3 +11,50 @@
 #i) K-Means.
 #ii) AGNES.
 #iii) DBSCAN.
+
+cols = ["Channel","Region","Fresh","Milk","Grocery","Frozen","Detergents_Paper","Delicassen"]
+df = pd.read_csv("data.csv",usecols=cols)
+#print(df.describe())
+pp = PreProcessing()
+
+
+
+
+
+
+
+
+def get_visual_data():
+    pp.convert_categorical(df,"Channel",[0.5,1.5,2.5],['1','2'])
+    sns.catplot(x="Channel",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Region",[0.5,1.5,2.5,3.5],['1','2','3'])
+    sns.catplot(x="Region",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Fresh",[0,12000,24000,36000,120000],['0-12000','12000-24000','24000-36000','36000-120000'])
+    sns.catplot(x="Fresh",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Milk",[0,2000,4000,8000,100000],['0-2000','2000-4000','4000-8000','8000-100000'])
+    sns.catplot(x="Milk",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Grocery",[0,2000,4000,8000,100000],['0-2000','2000-4000','4000-8000','8000-100000'])
+    sns.catplot(x="Grocery",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Frozen",[0,2000,4000,8000,100000],['0-2000','2000-4000','4000-8000','8000-100000'])
+    sns.catplot(x="Frozen",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Detergents_Paper",[0,1000,2500,5000,50000],['0-1000','1000-2500','2500-5000','5000-50000'])
+    sns.catplot(x="Detergents_Paper",kind='count',data=df)
+    plt.show()
+
+    pp.convert_categorical(df,"Delicassen",[0,1000,2500,5000,50000],['0-1000','1000-2500','2500-5000','5000-50000'])
+    sns.catplot(x="Delicassen",kind='count',data=df)
+    plt.show()
+    
+#get_visual_data()
---
+++ preprocessing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+class PreProcessing:
+    def get_stats(self, df):
+        for column in df.columns:
+            print('Statistics for Column' + str(column))
+            print(str(df[column].describe()) + '\n')
+
+    def convert_categorical(self,df, column, bins, labels):
+        df[column] = pd.cut(df[column], bins=bins, labels=labels)
