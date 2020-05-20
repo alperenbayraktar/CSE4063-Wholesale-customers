c20e2dbbcbcfaa217967c408110589d8791438cd
alperenbayraktar <alperenbayraktar96@gmail.com>
2020-05-20 18:32:33 +0300

    added to-do

---
+++ main.py
@@ -0,0 +1,8 @@
+##Use your dataset to construct 3 frequent pattern mining models as follows:
+#i) Apriori.
+#ii) FP-Growth.
+#iii) ECLAT.
+##Use your dataset to construct 3 clustering analysis methods as follows:
+#i) K-Means.
+#ii) AGNES.
+#iii) DBSCAN.
